09e39fdd988abc9c8e52a4f530695c9d070a59c2
takashi.mochizuki <mox2000@yahoo.co.jp>
2026-02-21 18:59:08 +0900

    Modify  CodeBuild config for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,8 +30,6 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: only-on-failure /* mox */,
-    screenshot: only-on-failure	/* mox */,
   },
 
   /* Configure projects for major browsers */
